36e6b9ad72475aba1a60527efb9bfda9f34c5605
yaswanth-2211 <145182914+yaswanth-2211@users.noreply.github.com>
2023-10-18 20:35:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-document.getElementById("band").innerHTML =touristSpots.sort()
+let news=[];
+let map={};
+for(i=0;i<touristSpots.length;i++){
+	let str=touristSpots[i].
+	str=str.replace(/\bthe\b|\ba\b|\ban\b/gi," ");
+	str=str.replace(/ +/gi, " ");
+	str=str.trim();
+	map[str]=touristSpots[i];
+	news.push(str);}
+
+news.sort();
+for(i=0;i<touristSpots.length;i++){
+	news[i]=map[news[i]];
+}
+document.getElementByID('band').innerHTML=news;
